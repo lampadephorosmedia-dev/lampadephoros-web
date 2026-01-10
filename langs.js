171755b042f3310dcdd437a05a09c6e097735f0a
lampadephorosmedia-dev <lampadephoros.media@gmail.com>
2026-01-10 23:47:55 +0100

    Update langs.js

--- langs.js
+++ langs.js
@@ -1,43 +1,43 @@
 /* FILE: langs.js */
 window.LANGS = [
   // Core
-  { code: "de",  label: "DE",  name: "Deutsch",             start: "start-de.html",  dir: "ltr" },
-  { code: "fr",  label: "FR",  name: "Français",            start: "start-fr.html",  dir: "ltr" },
-  { code: "en",  label: "EN",  name: "English",             start: "start-en.html",  dir: "ltr" },
-  { code: "pt",  label: "PT",  name: "Português",           start: "start-pt.html",  dir: "ltr" },
-  { code: "it",  label: "IT",  name: "Italiano",            start: "start-it.html",  dir: "ltr" },
+  { code: "de",  label: "DE",  name: "Deutsch",              start: "start-de.html",  dir: "ltr" },
+  { code: "fr",  label: "FR",  name: "Français",             start: "start-fr.html",  dir: "ltr" },
+  { code: "en",  label: "EN",  name: "English",              start: "start-en.html",  dir: "ltr" },
+  { code: "pt",  label: "PT",  name: "Português",            start: "start-pt.html",  dir: "ltr" },
+  { code: "it",  label: "IT",  name: "Italiano",             start: "start-it.html",  dir: "ltr" },
 
   // Europe add-ons
-  { code: "es",  label: "ES",  name: "Español",             start: "start-es.html",  dir: "ltr" },
-  { code: "pl",  label: "PL",  name: "Polski",              start: "start-pl.html",  dir: "ltr" },
-  { code: "ru",  label: "RU",  name: "Русский",             start: "start-ru.html",  dir: "ltr" },
-  { code: "nl",  label: "NL",  name: "Nederlands",          start: "start-nl.html",  dir: "ltr" },
-  { code: "ro",  label: "RO",  name: "Română",              start: "start-ro.html",  dir: "ltr" },
-  { code: "uk",  label: "UK",  name: "Українська",          start: "start-uk.html",  dir: "ltr" },
+  { code: "es",  label: "ES",  name: "Español",              start: "start-es.html",  dir: "ltr" },
+  { code: "pl",  label: "PL",  name: "Polski",               start: "start-pl.html",  dir: "ltr" },
+  { code: "ru",  label: "RU",  name: "Русский",              start: "start-ru.html",  dir: "ltr" },
+  { code: "nl",  label: "NL",  name: "Nederlands",           start: "start-nl.html",  dir: "ltr" },
+  { code: "ro",  label: "RO",  name: "Română",               start: "start-ro.html",  dir: "ltr" },
+  { code: "uk",  label: "UK",  name: "Українська",           start: "start-uk.html",  dir: "ltr" },
 
-  // Balkans (clean set)
-  { code: "hr",  label: "HR",  name: "Hrvatski",            start: "start-hr.html",  dir: "ltr" },
-  { code: "bs",  label: "BS",  name: "Bosanski",            start: "start-bs.html",  dir: "ltr" },
-  { code: "sr",  label: "SR",  name: "Srpski",              start: "start-sr.html",  dir: "ltr" },
+  // Balkans
+  { code: "hr",  label: "HR",  name: "Hrvatski",             start: "start-hr.html",  dir: "ltr" },
+  { code: "bs",  label: "BS",  name: "Bosanski",             start: "start-bs.html",  dir: "ltr" },
+  { code: "sr",  label: "SR",  name: "Srpski",               start: "start-sr.html",  dir: "ltr" },
 
   // Middle East / RTL
-  { code: "ar",  label: "AR",  name: "العربية",             start: "start-ar.html",  dir: "rtl" },
-  { code: "ur",  label: "UR",  name: "اردو",                start: "start-ur.html",  dir: "rtl" },
-  { code: "fa",  label: "FA",  name: "فارسی",               start: "start-fa.html",  dir: "rtl" },
-  { code: "tr",  label: "TR",  name: "Türkçe",              start: "start-tr.html",  dir: "ltr" },
+  { code: "ar",  label: "AR",  name: "العربية",              start: "start-ar.html",  dir: "rtl" },
+  { code: "ur",  label: "UR",  name: "اردو",                 start: "start-ur.html",  dir: "rtl" },
+  { code: "fa",  label: "FA",  name: "فارسی",                start: "start-fa.html",  dir: "rtl" },
+  { code: "tr",  label: "TR",  name: "Türkçe",               start: "start-tr.html",  dir: "ltr" },
 
   // Asia big reach
-  { code: "zh",  label: "ZH",  name: "中文",                 start: "start-zh.html",  dir: "ltr" },
-  { code: "hi",  label: "HI",  name: "हिन्दी",              start: "start-hi.html",  dir: "ltr" },
-  { code: "bn",  label: "BN",  name: "বাংলা",               start: "start-bn.html",  dir: "ltr" },
-  { code: "id",  label: "ID",  name: "Bahasa Indonesia",    start: "start-id.html",  dir: "ltr" },
-  { code: "fil", label: "FIL", name: "Filipino",            start: "start-fil.html", dir: "ltr" },
-  { code: "ja",  label: "JA",  name: "日本語",               start: "start-ja.html",  dir: "ltr" },
-  { code: "ko",  label: "KO",  name: "한국어",               start: "start-ko.html",  dir: "ltr" },
-  { code: "vi",  label: "VI",  name: "Tiếng Việt",          start: "start-vi.html",  dir: "ltr" },
+  { code: "zh",  label: "ZH",  name: "中文",                  start: "start-zh.html",  dir: "ltr" },
+  { code: "hi",  label: "HI",  name: "हिन्दी",               start: "start-hi.html",  dir: "ltr" },
+  { code: "bn",  label: "BN",  name: "বাংলা",                start: "start-bn.html",  dir: "ltr" },
+  { code: "id",  label: "ID",  name: "Bahasa Indonesia",     start: "start-id.html",  dir: "ltr" },
+  { code: "fil", label: "FIL", name: "Filipino",             start: "start-fil.html", dir: "ltr" },
+  { code: "ja",  label: "JA",  name: "日本語",                start: "start-ja.html",  dir: "ltr" },
+  { code: "ko",  label: "KO",  name: "한국어",                start: "start-ko.html",  dir: "ltr" },
+  { code: "vi",  label: "VI",  name: "Tiếng Việt",           start: "start-vi.html",  dir: "ltr" },
 
-  // Extra (Singapore + Kurdish)
-  { code: "ms",  label: "MS",  name: "Bahasa Melayu",       start: "start-ms.html",  dir: "ltr" },
-  { code: "ta",  label: "TA",  name: "தமிழ்",               start: "start-ta.html",  dir: "ltr" },
-  { code: "ku",  label: "KU",  name: "Kurdî (Kurmancî)",    start: "start-ku.html",  dir: "ltr" }
+  // Singapore + Kurdish
+  { code: "ms",  label: "MS",  name: "Bahasa Melayu",        start: "start-ms.html",  dir: "ltr" },
+  { code: "ta",  label: "TA",  name: "தமிழ்",                start: "start-ta.html",  dir: "ltr" },
+  { code: "ku",  label: "KU",  name: "Kurdî (Kurmancî)",     start: "start-ku.html",  dir: "ltr" }
 ];
